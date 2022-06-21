89733da92ba21d1b2d537184ab87e56baa5612c4
Joaquin <jaltable@fi.uba.ar>
2022-06-20 21:21:08 -0300

    Acerca: Añadido boton y caja para añadir texto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AcercaComponent } from './acerca/acerca.component';
@@ -19,7 +20,8 @@ import { ProjectsComponent } from './projects/projects.component'
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
